2ad1832a81a340263c432776c7b3dbdca0f763ab
daimaxiaxie <zhang971207@hotmail.com>
2023-11-04 21:44:54 +0800

    v0.1.0

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"golang.org/x/net/webdav"
+	"net/http"
+)
+
+var (
+	path = flag.String("path", "./dav", "--path=/www/dav/")
+	port = flag.Int("port", 80, "--port=80")
+)
+
+func main() {
+	flag.Parse()
+	mux := http.NewServeMux()
+	handler := &webdav.Handler{
+		Prefix:     "/dav/",
+		FileSystem: webdav.Dir(*path),
+		LockSystem: webdav.NewMemLS(),
+	}
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		username, password, ok := req.BasicAuth()
+		if !ok {
+			fmt.Println("no login")
+		}
+		fmt.Println("login: ", username, password)
+		handler.ServeHTTP(w, req)
+		return
+	})
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), mux); err != nil {
+		fmt.Println(err)
+	}
+}
